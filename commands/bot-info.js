c00f5f1a9d51706d16793d0fb142dee3e65c7fc1
Biagio <biagio.santori@gmail.com>
2020-04-23 00:13:21 +0100

    NEW VERSION TEST

---
+++ commands/bot-info.js
@@ -0,0 +1,56 @@
+const Util = require('./../modules/util')
+const Logger = new Util.Logger();
+const bot = require('../bot')
+
+/**
+ * Command: bot-info
+ * Description: Gives you some information about the bot.
+ * */
+module.exports = {
+	name: 'bot-info',
+	description: 'Gives you some information about the bot.',
+	execute(message, args, config) {
+		// Check in what type of channel the command was executed
+		if(message.channel.type === 'dm' || message.channel.type === 'group') {
+			Logger.info(`${config.PREFIX + this.name} used in a private ${message.channel.type}.`)
+		}
+		else{
+			Logger.info(`${config.PREFIX + this.name} used on ${message.guild.name} (${message.guild.id}; ${message.guild.memberCount} users)`)
+		}
+
+		message.channel.send({
+			embed: {
+				author: {
+					name: message.client.user.username,
+					icon_url: message.client.user.avatarURL(),
+					url: 'https://github.com/MiddayClouds',
+				},
+				description: `*If you need help, type **${config.PREFIX}help***`,
+				color: 3447003,
+				fields: [
+					{
+						name: 'Website:',
+						value: 'https://github.com/MiddayClouds',
+						inline: false,
+					},
+					{
+						name: 'Servers',
+						value: message.client.guilds.cache.size,
+						inline: true,
+					},
+					{
+						name: 'Users',
+						value: bot.totalMembers(),
+						inline: true,
+					},
+					{
+						name: 'Version',
+						value: config.VERSION,
+						inline: false,
+					},
+				],
+				timestamp: new Date(),
+			},
+		})
+	},
+}
