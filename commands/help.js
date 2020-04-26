941544292c209c10192f44dc3f9eda3eae105cef
Biagio <biagio.santori@gmail.com>
2020-04-27 00:52:31 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -17,7 +17,7 @@ module.exports = {
     if (!args[1]) {
       data.push('Here\'s a list of all my commands:');
       data.push(commands.map(command => command.name).join(', '));
-      data.push(`\nYou can send \`${prefix}help [command name]\` to get info on a specific command!`);
+      data.push(`\nYou can send \`${PREFIX}help [command name]\` to get info on a specific command.`);
 
       return message.author.send(data, { split: true })
       .then(() => {
