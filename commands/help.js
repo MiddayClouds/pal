089bd0aec0c7b5cfdb4b4930ea12d77b53f8ebb6
Biagio <biagio.santori@gmail.com>
2020-04-27 00:48:44 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,6 +1,7 @@
 // Any module required will be written up here
 const Util = require('./../modules/util')
 
+const fs = require('fs');
 /**
  * Command: help
  * Description: Gives out a list of bot commands.
@@ -10,32 +11,41 @@ module.exports = {
 	name: 'help',
 	description: 'Gives out a list of bot commands.',
 	execute(message, args, config) {
-    const command = args[0].slice(config.PREFIX.length,)
-    const rawArgument = args.join(' ')
-    const argument = rawArgument.replace(config.PREFIX + command + ' ', '')
+    const data = [];
+    const { commands } = message.client;
 
-    // Put command here...
+    if (!args.length) {
+      data.push('Here\'s a list of all my commands:');
+      data.push(commands.map(command => command.name).join(', '));
+      data.push(`\nYou can send \`${prefix}help [command name]\` to get info on a specific command!`);
 
-
-    getTotalCommands = new collection
-    const totalCommands = fs.readdirSync('./commands').filter(file => file.endsWith('.js'))
-    for (const file of totalCommands) {
-    	const command = require(`./commands/${file}`);
-    	// set a new item in the Collection
-    	// with the key as the command name and the value as the exported module
-    	getTotalCommands.commands.set(command.name, command);
-    	// Check if any alias does exist and add if they do
-    	if(command.alias) {
-    		for(const alias of command.alias) {
-    			getTotalCommands.commands.set(alias, command)
-    		}
-      }
+      return message.author.send(data, { split: true })
+      .then(() => {
+        if (message.channel.type === 'dm') return;
+        message.reply('I\'ve sent you a DM with all my commands!');
+      })
+      .catch(error => {
+        console.error(`Could not send help DM to ${message.author.tag}.\n`, error);
+        message.reply('it seems like I can\'t DM you!');
+      });
     }
 
-    console.log(getTotalCommands)
+    const name = args[0].toLowerCase();
+		const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
 
+		if (!command) {
+			return message.reply('that\'s not a valid command!');
+		}
 
+		data.push(`**Name:** ${command.name}`);
 
+		if (command.aliases) data.push(`**Aliases:** ${command.aliases.join(', ')}`);
+		if (command.description) data.push(`**Description:** ${command.description}`);
+		if (command.usage) data.push(`**Usage:** ${prefix}${command.name} ${command.usage}`);
+
+		data.push(`**Cooldown:** ${command.cooldown || 3} second(s)`);
+
+		message.channel.send(data, { split: true });
 
 	},
 }
