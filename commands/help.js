c74357dc2d55f8c4a6adbfd2ca983423d452acb2
Biagio <biagio.santori@gmail.com>
2020-04-27 00:51:42 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -41,9 +41,9 @@ module.exports = {
 
 		if (command.aliases) data.push(`**Aliases:** ${command.aliases.join(', ')}`);
 		if (command.description) data.push(`**Description:** ${command.description}`);
-		if (command.usage) data.push(`**Usage:** ${prefix}${command.name} ${command.usage}`);
+		if (command.usage) data.push(`**Usage:** ${PREFIX}${command.name} ${command.usage}`);
 
-		data.push(`**Cooldown:** ${command.cooldown || 3} second(s)`);
+		//data.push(`**Cooldown:** ${command.cooldown || 3} second(s)`);
 
 		message.channel.send(data, { split: true });
 
