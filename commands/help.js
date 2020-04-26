53f7e91f24cbf049d3a62b1f3936e0e5a0dc89b2
Biagio <biagio.santori@gmail.com>
2020-04-27 00:54:15 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -17,7 +17,7 @@ module.exports = {
     if (!args[1]) {
       data.push('Here\'s a list of all my commands:');
       data.push(commands.map(command => command.name).join(', '));
-      data.push(`\nYou can send \`${PREFIX}help [command name]\` to get info on a specific command.`);
+      data.push(`\nYou can send \`${config.PREFIX}help [command name]\` to get info on a specific command.`);
 
       return message.author.send(data, { split: true })
       .then(() => {
@@ -30,7 +30,7 @@ module.exports = {
       });
     }
 
-    const name = args[0].toLowerCase();
+    const name = args[2].toLowerCase();
 		const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
 
 		if (!command) {
@@ -41,7 +41,7 @@ module.exports = {
 
 		if (command.aliases) data.push(`**Aliases:** ${command.aliases.join(', ')}`);
 		if (command.description) data.push(`**Description:** ${command.description}`);
-		if (command.usage) data.push(`**Usage:** ${PREFIX}${command.name} ${command.usage}`);
+		if (command.usage) data.push(`**Usage:** ${config.PREFIX}${command.name} ${command.usage}`);
 
 		//data.push(`**Cooldown:** ${command.cooldown || 3} second(s)`);
 
