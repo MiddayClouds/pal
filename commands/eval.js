55ed16cbbf5088a4f9b10b2f9f703993c19aeed7
Biagio <biagio.santori@gmail.com>
2020-05-16 22:40:09 +0100

    New command and some bug fixes
    Added new command:
    - poll, allows user to create a poll
    
    Bug Issues:
    - Fixed issue where when bot joined a new guild it would throw an error.
    - Fixed an issue where the invite would send out an invalid id making the bot invite useless
    - Added some style and an option to have a clean eval
    - Adjusted log when bot leaves a guild

--- commands/eval.js
+++ commands/eval.js
@@ -7,18 +7,30 @@
 // However it's, like, super ultra useful for troubleshooting and doing stuff
 // you don't want to put in a command.
 exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
-  const code = args.join(" ");
-  try {
-    const evaled = eval(code);
-    const clean = await client.clean(client, evaled);
-    message.channel.send(`\`\`\`js\n${clean}\n\`\`\``);
-  } catch (err) {
-    message.channel.send(`\`ERROR\` \`\`\`xl\n${await client.clean(client, err)}\n\`\`\``);
+  if (args[0] == "clean") {
+    try {
+      args.shift()
+      const code = args.join(" ");
+      const evaled = eval(code);
+      const clean = await client.clean(client, evaled);
+    } catch (err) {
+      message.channel.send(`\`ERROR\` \`\`\`xl\n${await client.clean(client, err)}\n\`\`\``);
+    }
+  } else {
+    try {
+      const code = args.join(" ");
+      const evaled = eval(code);
+      const clean = await client.clean(client, evaled);
+      message.channel.send(":inbox_tray: (Input) ```js\n"+evaled+"```")
+      message.channel.send(":outbox_tray: (Output) ```js\n"+clean+"```")
+    } catch (err) {
+      message.channel.send(`\`ERROR\` \`\`\`xl\n${await client.clean(client, err)}\n\`\`\``);
+    }
   }
 };
 
 exports.conf = {
-  enabled: false,
+  enabled: true,
   guildOnly: false,
   aliases: [],
   permLevel: "Bot Owner",
