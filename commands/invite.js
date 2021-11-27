a5cbc155a1bc4f12f22dc2d52f2e21d8918c67b0
Restyled.io <commits@restyled.io>
2021-11-27 00:21:10 +0000

    Restyled by prettier

--- commands/invite.js
+++ commands/invite.js
@@ -1,15 +1,15 @@
-exports.run = (client, message, args,
-               level) => { // eslint-disable-line no-unused-vars
+exports.run = (client, message, args, level) => {
+  // eslint-disable-line no-unused-vars
   message.channel.send({
-    embed : {
-      color : 3447003,
-      fields : [
+    embed: {
+      color: 3447003,
+      fields: [
         {
-          name : "Bot Invite: ",
-          value :
-              "[Clock here](https://discord.com/oauth2/authorize?client_id=" +
-                  client.user.id +
-                  "&scope=bot%20applications.commands&permissions=805694544) to add the bot to your server.",
+          name: "Bot Invite: ",
+          value:
+            "[Clock here](https://discord.com/oauth2/authorize?client_id=" +
+            client.user.id +
+            "&scope=bot%20applications.commands&permissions=805694544) to add the bot to your server.",
         },
       ],
     },
@@ -17,16 +17,16 @@ exports.run = (client, message, args,
 };
 
 exports.conf = {
-  enabled : true,
-  guildOnly : false,
-  aliases : [ "add" ],
-  permLevel : "User",
-  cooldown : 5
+  enabled: true,
+  guildOnly: false,
+  aliases: ["add"],
+  permLevel: "User",
+  cooldown: 5,
 };
 
 exports.help = {
-  name : "invite",
-  category : "Miscelaneous",
-  description : "Outputs a bot invite link",
-  usage : "`invite`"
+  name: "invite",
+  category: "Miscelaneous",
+  description: "Outputs a bot invite link",
+  usage: "`invite`",
 };
